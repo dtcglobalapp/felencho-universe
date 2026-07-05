ccd7487e8cbdfad8fea55918b03bece37cc69853
MARTIN F DURAN <martinfduran@mac.mynetworksettings.com>
2026-07-05 08:39:34 -0400

    Upgrade PresenceController with subscriptions

--- lib/PresenceController.ts
+++ lib/PresenceController.ts
@@ -1,33 +1,68 @@
+export type PresenceMode = "presence" | "live";
+
 export type PresenceState =
   | "sleeping"
   | "waking"
   | "thinking"
   | "speaking";
 
-class PresenceController {
+type Listener = () => void;
 
+class PresenceController {
+  private mode: PresenceMode = "presence";
   private state: PresenceState = "sleeping";
+
+  private listeners: Listener[] = [];
+
+  subscribe(listener: Listener) {
+    this.listeners.push(listener);
+
+    return () => {
+      this.listeners = this.listeners.filter(
+        (item) => item !== listener
+      );
+    };
+  }
+
+  private notify() {
+    this.listeners.forEach((listener) => listener());
+  }
+
+  getMode() {
+    return this.mode;
+  }
 
   getState() {
     return this.state;
   }
 
-  sleep() {
+  setPresence() {
+    this.mode = "presence";
     this.state = "sleeping";
+    this.notify();
   }
 
-  wake() {
+  setLive() {
+    this.mode = "live";
     this.state = "waking";
+    this.notify();
   }
 
   think() {
     this.state = "thinking";
+    this.notify();
   }
 
   speak() {
     this.state = "speaking";
+    this.notify();
   }
 
+  sleep() {
+    this.state = "sleeping";
+    this.mode = "presence";
+    this.notify();
+  }
 }
 
 export const presenceController = new PresenceController();
